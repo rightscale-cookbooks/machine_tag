a110909b1ae3cf5050059035ac297c72f92e161b
Douglas Thrift <douglas.thrift@rightscale.com>
2013-12-10 13:28:05 -0800

    Get ready to release v1.0.1.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'cookbooks@rightscale.com'
 license          'Apache 2.0'
 description      'Installs/Configures machine_tag'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 recipe 'machine_tag::default', "Installs the 'machine_tag' gem used by the helpers."
