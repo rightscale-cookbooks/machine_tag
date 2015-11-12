b0aa16bc65912bc01e3dae4a32e6924b896bcf67
Curt Wilhelm <curt@rightscale.com>
2015-11-12 11:33:51 -0700

    update tests

--- spec/unit_test/machine_tag_spec.rb
+++ spec/unit_test/machine_tag_spec.rb
@@ -56,6 +56,7 @@ describe Chef::MachineTag do
 
   describe "::factory" do
     let(:shell_out) { Struct.new(:exitstatus) }
+    let(:stdout) { Struct.new(:stdout) }
 
     context "the rs_tag utility is in the path" do
       it "should return an object of MachineTagRightscale class" do
@@ -70,16 +71,28 @@ describe Chef::MachineTag do
         node.set['hostname'] = 'some_host'
         node.set['machine_tag']['vagrant_tag_cache_dir'] = '/vagrant/machine_tag_cache/'
         Chef::MachineTag.should_receive(:shell_out).with('which', 'rs_tag').and_return(shell_out.new(1))
+        Chef::MachineTag.should_receive(:shell_out).with('which', 'rightlink').and_return(shell_out.new(1))
         Chef::MachineTag.factory(node).should be_an_instance_of(Chef::MachineTagVagrant)
       end
     end
 
-    context "the rs_tag utility is not in the path" do
+    context "the rs_tag or rightlink utility is not in the path" do
       it "should raise exception if cloud provider not found in the node" do
         Chef::MachineTag.should_receive(:shell_out).with('which', 'rs_tag').and_return(shell_out.new(1))
+        Chef::MachineTag.should_receive(:shell_out).with('which', 'rightlink').and_return(shell_out.new(1))
         expect do
           Chef::MachineTag.factory(node)
         end.to raise_error(RuntimeError)
+      end
+    end
+    
+    context "rightlink 10 is the provider" do
+      it "should return an object of MachineTagRl10 class" do
+
+        Chef::MachineTag.should_receive(:shell_out).with('which', 'rs_tag').and_return(shell_out.new(1))
+        Chef::MachineTag.should_receive(:shell_out).with('which', 'rightlink').and_return(shell_out.new(0))
+        Chef::MachineTag.should_receive(:shell_out).with('rightlink', '-version').and_return(stdout.new("RightLink 10.2.1"))
+        Chef::MachineTag.factory(node).should be_an_instance_of(Chef::MachineTagRl10)
       end
     end
   end
